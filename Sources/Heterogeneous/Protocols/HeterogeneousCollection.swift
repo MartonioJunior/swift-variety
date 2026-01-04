05db85d8785397c595b135afcc5a404c3cc85fb6
Martônio Júnior <martoniojunior@hotmail.com>
2026-01-04 15:03:13 -0300

    feat(Heterogeneous): `HeterogeneousCollection`
    - New fetch methods `fetch(on:)` and `scope(for:)`
    - Added default implementation for `@dynamicMemberLookup` on string keys

--- Sources/Heterogeneous/Protocols/HeterogeneousCollection.swift
+++ Sources/Heterogeneous/Protocols/HeterogeneousCollection.swift
@@ -16,4 +16,19 @@ public extension HeterogeneousCollection {
     subscript<T>(_ valueType: T.Type = T.self, _ key: Key) -> T? {
         try? fetch(.init(key, valueType))
     }
+
+    func fetch<T>(on keyPath: KeyPath<T.Type, Key>, as type: T.Type) throws(HeterogeneousKeyError) -> T? {
+        try fetch(.init(type[keyPath: keyPath], type))
+    }
+
+    func scope<H: HeterogeneousCollection>(for key: HeterogeneousKey<Key, H>) throws(HeterogeneousKeyError) -> H {
+        try fetch(key)
+    }
+}
+
+// MARK: Self.Key == String
+public extension HeterogeneousCollection where Key == String {
+    subscript<T>(dynamicMember member: Key) -> T? {
+        self[T.self, member]
+    }
 }
